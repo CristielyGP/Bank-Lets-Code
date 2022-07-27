5bf7c212b4be2848b64e905d0b903fd745a5d4b1
Cristiely Gomes Pires <cgpires@bb.com.br>
2022-07-27 18:20:48 -0300

    implementação básica classes clientes

--- src/clientes/ClientePessoaFisica.java
+++ src/clientes/ClientePessoaFisica.java
@@ -1,5 +1,19 @@
 package clientes;
 
-public class ClientePessoaFisica extends Cliente{
-    
+public class ClientePessoaFisica extends Cliente{     
+     String cpf;
+
+    public ClientePessoaFisica(String nome, String cpf, String telefone){
+        super(nome,telefone);
+        this.cpf = cpf;
+        
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
