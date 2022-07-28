3035798825e6362b6d3e3cbe54cc4fa4529cef56
F1381759 Artur Bezerra Delabio Ferraz <artur.delabio@bb.com.br>
2022-07-28 17:23:21 -0300

    fix: inclusão de regra de negócio que impede que pessoas jurídicas sejam capazes de criar conta poupança

--- src/contas/ContaPoupanca.java
+++ src/contas/ContaPoupanca.java
@@ -1,10 +1,11 @@
 package contas;
 
 import clientes.Cliente;
+import clientes.ClientePessoaFisica;
 
 public class ContaPoupanca extends Conta{
 
-    public ContaPoupanca(int numero, int agencia, Cliente titular) {
+    public ContaPoupanca(int numero, int agencia, ClientePessoaFisica titular) {
         super(numero, agencia, titular);
     }
     
